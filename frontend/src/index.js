a55bfa55186b7c797885cd46235f08f5e61c7483
unknown <tiokazami@gmail.com>
2023-05-19 00:01:39 +0700

    update 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,29 @@
 import './CartScreen.css';
 
+// components
+import CartItem from '../components/CartItem';
+
 const CartScreen = () => {
-    return <div className="cartscreen">CartScreen</div>
+    return <div className="cartscreen">
+        <div className='cartscreen__left'>
+            <h2>Your Cart</h2>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+        </div>
+        <div className="cartscreen__right">
+            <div className='cartscreen__info'>
+                <p>Sub total (0) items</p>
+                <p>$99.99</p>
+            </div>
+            <div>
+                <button>Procced to checkout</button>
+            </div>
+        </div>
+    </div>
 };
 
 export default CartScreen;
